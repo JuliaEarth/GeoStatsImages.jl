f5b0eb48bc7e58d2d98d3b356f5e13725f47ab0e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-12-13 15:56:25 -0800

    Add basic tests with test sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,12 @@
 using GeoStatsImages
 using Base.Test
 
-# no tests
+@testset "Basic checks" begin
+  # all available images loaded as arrays
+  for identifier in GeoStatsImages.available
+    @test isa(training_image(identifier), AbstractArray)
+  end
+
+  # throws on non existing identifier
+  @test_throws AssertionError training_image("NonExistingIdentifier")
+end
