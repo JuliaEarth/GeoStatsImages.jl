ab2724fafc6f2f3b296c8202d95c3ae4974678d9
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-01-16 21:06:06 -0800

    Add test for available identifiers

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,17 @@ using GeoStatsImages
 using Base.Test
 
 @testset "Basic checks" begin
-  # all available images loaded as arrays
+  # all images loaded as arrays
   for identifier in GeoStatsImages.identifiers
     @test isa(training_image(identifier), AbstractArray)
   end
 
   # throws on non existing identifier
   @test_throws AssertionError training_image("NonExistingIdentifier")
+
+  # identifiers are printed
+  rd, wr = redirect_stdout()
+  GeoStatsImages.available()
+  output = readavailable(rd)
+  @test !isempty(output)
 end
