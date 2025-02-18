8b15dbd9d12a2df79b8d28837928b7edefb19c6f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-02-18 06:35:14 -0300

    Increase test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,11 @@ using GeoTables
 using Test
 
 @testset "Basic checks" begin
+  # utility functions
+  @test GeoStatsImages.isdata("foo.gslib")
+  @test !GeoStatsImages.isdata("foo.csv")
+  @test GeoStatsImages.id("foo.gslib") == "foo"
+
   # all images loaded as arrays
   for id in GeoStatsImages.available()
     @test geostatsimage(id) isa GeoTable
